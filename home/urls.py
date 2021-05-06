93a364bc89aebe82179bbe896649b7f1ad1724cf
Jakub1994 <ostach2094@gmail.com>
2021-05-06 00:21:04 +0000

    admin product,category add

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="home")
+    path('', views.index, name="home"),
+    path('', views.all_products, name='home'),
+    path('', views.product_detail, name='home'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 # Create your views here.
-
 
 def index(request):
     """A view to return the index page"""
 
     return render(request, 'home/index.html')
+
+
+def all_products(request):
+    """A view to return the index page"""
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'home/index.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'home/index.html', context)
