d84956a4c25ed3eb1b092dad44eeee479abd978a
Jakub1994 <ostach2094@gmail.com>
2021-05-06 13:11:59 +0000

    products changes

--- home/views.py
+++ home/views.py
@@ -1,33 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render
 
 # Create your views here.
+
 
 def index(request):
     """A view to return the index page"""
 
     return render(request, 'home/index.html')
-
-
-def all_products(request):
-    """A view to return the index page"""
-
-    products = Product.objects.all()
-
-    context = {
-        'products': products,
-    }
-
-    return render(request, 'home/index.html', context)
-
-
-def product_detail(request, product_id):
-    """ A view to show individual product details """
-
-    product = get_object_or_404(Product, pk=product_id)
-
-    context = {
-        'product': product,
-    }
-
-    return render(request, 'home/index.html', context)
